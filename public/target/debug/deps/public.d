4535085cade8655379724cd0179cddf483aff9fc
Carlitos <juanpana10ospina@gmail.com~>
2025-07-05 16:48:43 -0500

    new main

---
+++ public/target/debug/deps/public.d
@@ -0,0 +1,5 @@
+C:\Users\LENOVO\Documents\MuseScore3\Imágenes\pagina web\public\target\debug\deps\public.d: src\main.rs
+
+C:\Users\LENOVO\Documents\MuseScore3\Imágenes\pagina web\public\target\debug\deps\public.exe: src\main.rs
+
+src\main.rs:
---
+++ public/target/debug/public.d
@@ -0,0 +1,1 @@
+C:\Users\LENOVO\Documents\MuseScore3\Imágenes\pagina\ web\public\target\debug\public.exe: C:\Users\LENOVO\Documents\MuseScore3\Imágenes\pagina\ web\public\src\main.rs
