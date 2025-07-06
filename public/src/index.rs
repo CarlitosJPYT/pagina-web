4e9c72a340bbf5d2321d0a8c593cac4ee879ce9c
Carlitos <juanpana10ospina@gmail.com~>
2025-07-05 22:04:15 -0500

    add merge/push

--- public/src/index.rs
+++ public/src/index.rs
@@ -1,6 +1,6 @@
 fn main(){
     let edad = 24;
-    if edad =< 5{println!("Eres un bebé.");}
+    if edad <= 5{println!("Eres un bebé.");}
     else if edad >= 6 && =< 12{println!("Eres un niño.");}
     else if edad >= 13 && =< 17{println!("Eres un adolescente.");}
     else if edad >= 18 && =< 49{println!("Eres un adulto.");}
