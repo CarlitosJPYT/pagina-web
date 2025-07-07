baced0889a97dbb08d808026952ebac609e6fdd2
Carlitos <juanpana10ospina@gmail.com~>
2025-07-07 11:31:08 -0500

    pull array

---
+++ src/src/index.rs
@@ -0,0 +1,17 @@
+fn main(){
+    let temperatura = 25;
+    if temperatura < 25{celsius(20);}
+    else if temperatura = 25{
+        let celsius = (temperatura - 32) * 5/9;
+        println!("La temperatura es de {celsius}");
+    }
+    else {Fahrenheit();}
+}
+fn celsius(temperatura: i32){
+    let resultado = temperatura * 5/9 + 32;
+    println!("La temperatura es de {resultado}");
+}
+fn Fahrenheit(temperatura: i32){
+    let operacion = temperatura - 343;
+    println!("La temperatura es de {operacion}");
+}
