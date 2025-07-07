93a140162f4c1f4abd94796821644a8ceadd0f64
Carlitos <juanpana10ospina@gmail.com~>
2025-07-07 11:38:03 -0500

    continue

--- src/src/index.rs
+++ src/src/index.rs
@@ -5,13 +5,13 @@ fn main(){
         let celsius = (temperatura - 32) * 5/9;
         println!("La temperatura es de {celsius}");
     }
-    else {Fahrenheit(50);}
+    else {fahrenheit(50);}
 }
 fn celsius(temperatura: i32){
     let resultado = temperatura * 5/9 + 32;
     println!("La temperatura es de {resultado}");
 }
-fn Fahrenheit(temperatura: i32){
+fn fahrenheit(temperatura: i32){
     let operacion = temperatura - 343;
     println!("La temperatura es de {operacion}");
 }
