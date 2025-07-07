ec46fc97dcce5d5954ae81c2dabc1bcf97f6338c
Carlitos <juanpana10ospina@gmail.com~>
2025-07-07 10:44:55 -0500

    add array

--- public/src/index.rs
+++ public/src/index.rs
@@ -1,8 +1,8 @@
 fn main(){
     let edad = 24;
     if edad <= 5{println!("Eres un bebé.");}
-    else if edad >= 6 && <= 12{println!("Eres un niño.");}
-    else if edad >= 13 && <= 17{println!("Eres un adolescente.");}
-    else if edad >= 18 && <= 49{println!("Eres un adulto.");}
+    else if edad >= 6 && edad <= 12{println!("Eres un niño.");}
+    else if edad >= 13 && edad <= 17{println!("Eres un adolescente.");}
+    else if edad >= 18 && edad <= 49{println!("Eres un adulto.");}
     else{println!("Eres un adulto mayor.")}
 }
